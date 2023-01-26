c729d2881fd1bf9da56e35751d8558623db27db6
rifat32 <rifatbilalphilips@gmail.com>
2023-01-26 20:15:47 +0600

    env

--- src/utils/backend.server.js
+++ src/utils/backend.server.js
@@ -1,6 +1,6 @@
 
    export const BACKEND = "https://mughalsignandprint.co.uk/garage-management";
-   // export const BACKEND = "http://127.0.0.1:8000";
+    // export const BACKEND = "http://127.0.0.1:8000";
 
 export const BACKEND_API = `${BACKEND}/api`;
 // export const http = "https"
